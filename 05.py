ae7404df36b823408d17ebb8ace211d9b8629167
leftmusic <163536132+leftmusic@users.noreply.github.com>
2024-03-22 12:25:43 +0000

    11

--- 05.py
+++ 05.py
@@ -1,10 +1,9 @@
 n = int(input())
-i = input()
-#n2 = input()
-l = []
-for i in range(n):
-#    x = abs(int(n2[i])-int(n2[i+1]))
-#    l.append(x)
-#l.sort()
-#print(l[0])
-    pass
+num = input().split()
+list=[]
+for i in num:
+    list.append(int(i))
+gap = []
+for i in range(1,n):
+    gap.append(list[i]-list[i-1])
+print(max(gap))
